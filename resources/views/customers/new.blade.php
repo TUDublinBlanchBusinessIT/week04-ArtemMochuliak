6654b8dd65ffc7e057e1f3b3b56cfcc934b558a8
Natalia Lishchenko <B00165618@mytudublin.ie>
2026-02-26 22:30:13 +0000

    Step 2 completed: Added Customer form view, controller, and routes

---
+++ resources/views/customers/new.blade.php
@@ -0,0 +1,6 @@
+<form method="POST" action="/customers/create">
+    @csrf
+    Enter your first name: <input type="text" name="firstname"><br>
+    Enter your surname: <input type="text" name="surname"><br>
+    <input type="submit" value="Submit">
+</form>
