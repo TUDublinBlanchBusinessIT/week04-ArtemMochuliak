f291a8900ee355e6e4feaf9403743b34dbf17651
Natalia Lishchenko <B00165618@mytudublin.ie>
2026-02-28 15:27:49 +0000

    Generated BREAD (CRUD) from existing member table using Infyom scaffold and simplified layout

---
+++ app/Repositories/MemberRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Member;
+use App\Repositories\BaseRepository;
+
+/**
+ * Class MemberRepository
+ * @package App\Repositories
+ * @version February 27, 2026, 4:34 pm UTC
+*/
+
+class MemberRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+        'firstname',
+        'surname',
+        'membertype',
+        'dateofbirth'
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return Member::class;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/customers/new', 'App\Http\Controllers\CustomerController@new');
 Route::post('/customers/create', 'App\Http\Controllers\CustomerController@create')->name('customers.create');
 Route::get('/customers/edit/{id}', 'App\Http\Controllers\CustomerController@edit');
 Route::post('/customers/update', 'App\Http\Controllers\CustomerController@update');
+
+Route::resource('members', MemberController::class);
